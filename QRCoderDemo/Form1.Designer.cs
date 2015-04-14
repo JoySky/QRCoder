1e66ab85efb648ae65a4f00bf04e172b3fd1d988
JoySky <JoySky@outlook.com>
2015-04-14 12:59:59 +0800

    汉化

--- QRCoderDemo/Form1.Designer.cs
+++ QRCoderDemo/Form1.Designer.cs
@@ -37,11 +37,11 @@
             // buttonGenerate
             // 
             this.buttonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonGenerate.Location = new System.Drawing.Point(228, 23);
+            this.buttonGenerate.Location = new System.Drawing.Point(228, 21);
             this.buttonGenerate.Name = "buttonGenerate";
-            this.buttonGenerate.Size = new System.Drawing.Size(62, 23);
+            this.buttonGenerate.Size = new System.Drawing.Size(62, 21);
             this.buttonGenerate.TabIndex = 0;
-            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.Text = "生成";
             this.buttonGenerate.UseVisualStyleBackColor = true;
             this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
             // 
@@ -49,11 +49,11 @@
             // 
             this.textBoxQRCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxQRCode.Location = new System.Drawing.Point(40, 23);
+            this.textBoxQRCode.Location = new System.Drawing.Point(40, 21);
             this.textBoxQRCode.Name = "textBoxQRCode";
-            this.textBoxQRCode.Size = new System.Drawing.Size(182, 20);
+            this.textBoxQRCode.Size = new System.Drawing.Size(182, 21);
             this.textBoxQRCode.TabIndex = 1;
-            this.textBoxQRCode.Text = "www.code-bude.net";
+            this.textBoxQRCode.Text = "https://www.google.com.hk";
             // 
             // pictureBoxQRCode
             // 
@@ -62,23 +62,23 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBoxQRCode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBoxQRCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxQRCode.Location = new System.Drawing.Point(40, 55);
+            this.pictureBoxQRCode.Location = new System.Drawing.Point(40, 51);
             this.pictureBoxQRCode.Name = "pictureBoxQRCode";
-            this.pictureBoxQRCode.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxQRCode.Size = new System.Drawing.Size(250, 231);
             this.pictureBoxQRCode.TabIndex = 2;
             this.pictureBoxQRCode.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 323);
+            this.ClientSize = new System.Drawing.Size(331, 298);
             this.Controls.Add(this.pictureBoxQRCode);
             this.Controls.Add(this.textBoxQRCode);
             this.Controls.Add(this.buttonGenerate);
-            this.MinimumSize = new System.Drawing.Size(347, 361);
+            this.MinimumSize = new System.Drawing.Size(347, 336);
             this.Name = "Form1";
-            this.Text = "QRCoder Demo - www.code-bude.net";
+            this.Text = "二维码生成工具";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).EndInit();
             this.ResumeLayout(false);
